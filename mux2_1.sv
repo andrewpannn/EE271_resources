9c880d3de6848a4429c093ef3412f1607c79569d
andrewpannn <107073044+andrewpannn@users.noreply.github.com>
2024-10-10 11:32:05 -0700

    add module , input/output, and assign comments

--- mux2_1.sv
+++ mux2_1.sv
@@ -1,6 +1,10 @@
+// 2:1 multiplexer
 module mux2_1(i0, i1, sel, out);
+    // inputs and outputs
     input logic i0, i1, sel;
     output logic out;
 
+    // assign output
     assign out = (i1 & sel) | (i0 & ~sel); 
+
 endmodule
